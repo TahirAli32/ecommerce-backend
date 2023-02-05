110a9819362f1656350f4196620ec65d293d6a4b
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2023-02-05 23:53:14 +0500

    ENV setup

--- config/database.js
+++ config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'ecommerce'),
+      database: env('DATABASE_NAME', 'dbname'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'password1131'),
+      password: env('DATABASE_PASSWORD', 'dbpassword'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
